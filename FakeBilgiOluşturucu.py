d827e977f2af67671bb7b97aed09067a3bd0551a
Ahmet Şirin <ahmet.sirin.34@outlook.com>
2024-05-23 15:59:08 +0300

    faker kütüphanesinin yüklenmesi gerekmektedir.

---
+++ FakeBilgiOluşturucu.py
@@ -0,0 +1,10 @@
+from faker import Faker
+
+faker = Faker()                 # ilk defa verilen fake veriler #
+
+print(faker.name())             #Joanna Perry
+print(faker.address())          #864 Valenzuela Ranch New Katelynstad, AZ 42501
+print(faker.email())            #tracy12@example.com
+print(faker.text())             #Suddenly stock stuff less commercial join. Firm which low purpose.
+
+
